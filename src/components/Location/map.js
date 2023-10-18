03566c7e2731fd9c83a643d72a32ec2211cc8ffb
blackgenius007 <truthin3dimension@gmail.com>
2023-10-18 04:15:27 +0100

    updated package.json

--- src/components/Location/map.js
+++ src/components/Location/map.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import config from '../../../config';
+ 
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 mapboxgl.accessToken =
